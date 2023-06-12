03d5d15c57ca5836c83685ad49a4a7c2942be511
yanglinbo <yanglinbo@huawei.com>
2023-06-12 17:55:01 +0800

    注释代码

--- paddle/fluid/inference/ascendie/convert/cast_op.cc
+++ paddle/fluid/inference/ascendie/convert/cast_op.cc
@@ -14,81 +14,81 @@ limitations under the License. */
 
 #include "paddle/fluid/inference/ascendie/convert/op_converter.h"
 
-namespace paddle {
-namespace inference {
-namespace ascendie {
+// namespace paddle {
+// namespace inference {
+// namespace ascendie {
 
-class CastOpConverter : public OpConverter {
- public:
-  void operator()(const framework::proto::OpDesc& op,
-                  const framework::Scope& scope,
-                  bool test_mode) override {
+// class CastOpConverter : public OpConverter {
+//  public:
+//   void operator()(const framework::proto::OpDesc& op,
+//                   const framework::Scope& scope,
+//                   bool test_mode) override {
 
-    VLOG(3) << "convert a cast op to ascendie";
-    framework::OpDesc op_desc(op, nullptr);
+//     VLOG(3) << "convert a cast op to ascendie";
+//     framework::OpDesc op_desc(op, nullptr);
 
-    Ascend::Tensor* input = engine_->GetITensor(op_desc.Input("X")[0]);
+//     Ascend::Tensor* input = engine_->GetITensor(op_desc.Input("X")[0]);
 
-    phi::ProtoDataType out_dtype = static_cast<phi::ProtoDataType>PADDLE_GET_CONST(int, op_desc.GetAttr("out_dtype"));
+//     phi::ProtoDataType out_dtype = static_cast<phi::ProtoDataType>PADDLE_GET_CONST(int, op_desc.GetAttr("out_dtype"));
 
-    AscendIE::CastLayer* cast = nullptr;
+//     AscendIE::CastLayer* cast = nullptr;
 
-    switch (out_dtype) {
-      case phi::ProtoDataType::BOOL:
-        cast = network.addCast(input, AscendIE::DataType::BOOL);
-        if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::BOOL);
-        break;
-      case phi::ProtoDataType::INT16:
-        cast = network.addCast(input, AscendIE::DataType::INT16);
-        if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::INT16);
-      case phi::ProtoDataType::INT32:
-        cast = network.addCast(input, AscendIE::DataType::INT32);
-        if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::INT32);
-      case phi::ProtoDataType::INT64:
-        cast = network.addCast(input, AscendIE::DataType::INT64);
-        if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::INT64);
-        break;
-      case phi::ProtoDataType::UINT8:
-        cast = network.addCast(input, AscendIE::DataType::UINT8);
-        if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::UINT8);
-        break;
-      case phi::ProtoDataType::INT8:
-        cast = network.addCast(input, AscendIE::DataType::INT8);
-        if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::INT8);
-        break;
-      case phi::ProtoDataType::FP16:
-        cast = network.addCast(input, AscendIE::DataType::FLOAT16);
-        if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::FLOAT16);
-        break;
-      // case phi::ProtoDataType::FP32:
-      // case phi::ProtoDataType::FP64:
-      // case phi::ProtoDataType::RAW:
-      // case phi::ProtoDataType::BF16:
-      // case phi::ProtoDataType::COMPLEX64:
-      // case phi::ProtoDataType::COMPLEX128:
-      //   LOG(ERROR) << "Unsupport type(" << out_dtype
-      //              << ") to a Ascend DataType";
-        break;
-      default:
-        LOG(ERROR) << "Unable to convert a fluid data type(" << out_dtype
-                   << ") to a Ascend DataType";
-        break;
-    }
+//     switch (out_dtype) {
+//       case phi::ProtoDataType::BOOL:
+//         cast = network.addCast(input, AscendIE::DataType::BOOL);
+//         if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::BOOL);
+//         break;
+//       case phi::ProtoDataType::INT16:
+//         cast = network.addCast(input, AscendIE::DataType::INT16);
+//         if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::INT16);
+//       case phi::ProtoDataType::INT32:
+//         cast = network.addCast(input, AscendIE::DataType::INT32);
+//         if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::INT32);
+//       case phi::ProtoDataType::INT64:
+//         cast = network.addCast(input, AscendIE::DataType::INT64);
+//         if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::INT64);
+//         break;
+//       case phi::ProtoDataType::UINT8:
+//         cast = network.addCast(input, AscendIE::DataType::UINT8);
+//         if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::UINT8);
+//         break;
+//       case phi::ProtoDataType::INT8:
+//         cast = network.addCast(input, AscendIE::DataType::INT8);
+//         if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::INT8);
+//         break;
+//       case phi::ProtoDataType::FP16:
+//         cast = network.addCast(input, AscendIE::DataType::FLOAT16);
+//         if(cast != nullptr) cast->getOutput(0)->setType(AscendIE::DataType::FLOAT16);
+//         break;
+//       // case phi::ProtoDataType::FP32:
+//       // case phi::ProtoDataType::FP64:
+//       // case phi::ProtoDataType::RAW:
+//       // case phi::ProtoDataType::BF16:
+//       // case phi::ProtoDataType::COMPLEX64:
+//       // case phi::ProtoDataType::COMPLEX128:
+//       //   LOG(ERROR) << "Unsupport type(" << out_dtype
+//       //              << ") to a Ascend DataType";
+//         break;
+//       default:
+//         LOG(ERROR) << "Unable to convert a fluid data type(" << out_dtype
+//                    << ") to a Ascend DataType";
+//         break;
+//     }
 
-    if(cast == nullptr) {
-      LOG(ERROR) << "cast is nullptr";
-      return;
-    }
-    network.SetOutput(cast->GetOutput(0));
+//     if(cast == nullptr) {
+//       LOG(ERROR) << "cast is nullptr";
+//       return;
+//     }
+//     network.SetOutput(cast->GetOutput(0));
 
-    auto output_name = op_desc.Output("Out")[0];
+//     auto output_name = op_desc.Output("Out")[0];
 
-    RreplenishLayerAndOutput(cast, "cast", {output_name}, test_mode);
-  }
-};
+//     RreplenishLayerAndOutput(cast, "cast", {output_name}, test_mode);
+//   }
+// };
 
-}  // namespace ascendie
-}  // namespace inference
-}  // namespace paddle
+// }  // namespace ascendie
+// }  // namespace inference
+// }  // namespace paddle
 
-REGISTER_ASCEND_OP_CONVERTER(cast, CastOpConverter);
+// REGISTER_ASCEND_OP_CONVERTER(cast, CastOpConverter);
